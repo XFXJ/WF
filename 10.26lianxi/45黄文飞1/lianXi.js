5e30e4f0afd6a9c84d7270d6d740eca88eb5ee2c
XFXJ <1454337412@qq.com>
2018-10-29 12:16:58 +0800

    asd
    asdasd

--- 10.26lianxi/45黄文飞1/lianXi.js
+++ 10.26lianxi/45黄文飞1/lianXi.js
@@ -1,23 +1,31 @@
 var timer;//设计定时计时器
 
 //启动定时器函数定义---若启动定时器动作单一，可以直接调用window定时器
-function startTimer(speed)
-{
-timer=window.setInterval(changeNum,speed);
-}
+// function startTimer(speed)
+// {
+timer=window.setInterval(changeNum,1000);
+// }
 //调用定时器，使程序从一开始就执行变化
-startTimer(100);
+// startTimer(100);
 //获取h2
-var h20bj=document.querySelector('h2');
-console.log(h20bj);
+var images1=document.querySelector('images')
+var images2=document.querySelector('#images')
+
+console.log(images1);
+console.log(images2);
+console.log(images2.children[3]);
+
+// images2.children[4].
+
 //设置当前变化的好码变量及初值
 var currentNo=0;
 //定义变化数字函数， 0到9，到9后在变化到0
 function changeNum()
 {
-if(currentNo < 9) currentNo++;
-else currentNo=0;
+if(currentNo < 8) currentNo++;
+else currentNo=1;
 h20bj.textContent=currentNo;
+h20bj.innerHTML='<img src="images/0'+currentNo+'.jpg" alt=""></img>';
 }
 // console.log(timer);
 
