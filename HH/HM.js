5b2df0deb1b76d1172200c5b162b555882631654
XFXJ <1454337412@qq.com>
2018-10-29 22:26:29 +0800

    aa
    人都是

--- HH/HM.js
+++ HH/HM.js
@@ -27,112 +27,149 @@ else
 searchBox.style.background="rgba(201,21,35,"+opacity+")";
 
     }
-    // 轮播图
+  
+    }
+    //轮播图
+window.onload=function()
+ {
     function banner()
+ {
+    var banner=document.querySelector('.hm_banner');
+    var w=banner.offsetWidth;
+    var imageBox=banner.querySelector('ul:first-child');
+    var pointBox=banner.querySelector('ul:last-child');
+    var points=pointBox.querySelector('li');
+    var addTransition=function()
     {
-        var banner=document.querySelector('.hm_banner');
-        var w=banner.offsetWidth;
-        var imageBox=banner.querySelector('ul:first-child');
-        var pointBox=banner.querySelector('ul:last-child');
-        var points=pointBox.querySelector('li');
-        var addTransition=function()
+        imageBox.style.webkitTransition="all .2s";
+        imageBox.style.transition="all .2s";
+    };
+    var removeTransition=function()
+    {
+          imageBox.style.webkitTransition="none";
+          imageBox.style.transition="none";
+    };
+    var setTranslateX=function(translateX)
+    {
+        imageBox.style.webkitTransform="translateX("+translateX+"px)";
+        imageBox.style.transform="translateX("+translateX+"px)";
+    };
+    var index=1;
+    var timer=setInterval(function()
+    {
+        index ++;
+        addTransition();
+        setTranslateX(-index*w);
+    },4000);
+    itcast.transitionEnd(imageBox,function()
+    {
+        console.log('transitionEnd');
+        if(index>=9)
         {
-            imageBox.style.webkitTransition="all .2s";
-            imageBox.style.transition="all .2s";
-        };
-        var removeTransition=function()
+            index=1;
+            removeTransition();
+            setTranslateX(-index*w);
+        }
+        else if(index<=0)
         {
-              imageBox.style.webkitTransition="none";
-              imageBox.style.transition="none";
-        };
-        var setTranslateX=function(translateX)
+            index=8;
+            removeTransition();
+            setTranslateX(-index*w);
+        }
+        setPpint();
+    });
+    var setPoint=function()
+    {
+        for(var i=0; i<points.length; i++)
         {
-            imageBox.style.webkitTransform="translateX("+translateX+"px)";
-            imageBox.style.transform="translateX("+translateX+"px)";
-        };
-        var index=1;
-        var timer=setInterval(function()
+            points[i].className=" ";
+        }
+        points[i].className="now";
+    }
+    var startX=0;
+    var moveX=0;
+    var distanceX=0;
+    var isMove=false;
+
+    imageBox.addEventListener('touchstart',function(e)
+    {
+        clearInterval(timer);
+        startX=e.touches[0].clientX;
+    });
+    imageBox.addEventListener('touchmove',function(e)
+    {
+        isMove=true;
+        moveX=e.touches[0].clientX;
+        distanceX=moveX-startX;
+        console,log(distanceX);
+        var currX=-index*w+distanceX;
+        removeTransition();
+        setTranslateX(currX);
+    });
+    imageBox.addEventListener('touchend',function(e)
+    {
+        if(isMove && (Math.abs(distanceX)>w/3))
+        {
+            if(distanceX>0)
+            {
+                index --;
+            }
+            else
+            {
+                index ++;
+            }
+            addTransition();
+            setTranslateX(-index*w);
+        }
+        else
+        {
+            addTransition();
+            setTranslateX(-index*w);
+        }
+        startX=0;
+        moveX=0;
+        distanceX=0;
+        isMove=false;
+        clearInterval(timer);
+        timer=setInterval(function()
         {
             index ++;
             addTransition();
             setTranslateX(-index*w);
-        },4000);
-        itcast.transitionEnd(imageBox,function()
+        },4000)
+    });
+}
+}
+window.onload=function()
+{
+    function downTime()
+    {
+        var time=5*60*60;
+        var timer=null;
+        var skTime=document.querySelector('.sk_time');
+        var spams=skTime.querySelector('span')
+        timer=setInterval(function()
         {
-            console.log('transitionEnd');
-            if(index>=9)
+            if(time<=0)
             {
-                index=1;
-                removeTransition();
-                setTranslateX(-index*w);
+                clearInterval(timer);
+                return false;
             }
-            else if(index<=0)
-            {
-                index=8;
-                removeTransition();
-                setTranslateX(-index*w);
-            }
-            setPpint();
-        });
-        var setPoint=function()
-        {
-            for(var i=0; i<points.length; i++)
-            {
-                points[i].className=" ";
-            }
-            points[i].className="now";
-        }
-        var startX=0;
-        var moveX=0;
-        var distanceX=0;
-        var isMove=false;
+            time --;
 
-        imageBox.addEventListener('touchstart',function(e)
-        {
-            clearInterval(timer);
-            startX=e.touches[0].clientX;
-        });
-        imageBox.addEventListener('touchmove',function(e)
-        {
-            isMove=true;
-            moveX=e.touches[0].clientX;
-            distanceX=moveX-startX;
-            console,log(distanceX);
-            var currX=-index*w+distanceX;
-            removeTransition();
-            setTranslateX(currX);
-        });
-        imageBox.addEventListener('touchend',function(e)
-        {
-            if(isMove && (Math.abs(distanceX)>w/3))
-            {
-                if(distanceX>0)
-                {
-                    index --;
-                }
-                else
-                {
-                    index ++;
-                }
-                addTransition();
-                setTranslateX(-index*w);
-            }
-            else
-            {
-                addTransition();
-                setTranslateX(-index*w);
-            }
-            startX=0;
-            moveX=0;
-            distanceX=0;
-            isMove=false;
-            clearInterval(timer);
-            timer=setInterval(function()
-            {
-                index ++;
-                addTransition();
-                setTranslateX(-index*w);
-            },4000)
-        });
+            var h=Math.floor(time/3600);
+            var m=Math.floor(time%3600/60);
+            var s=time%60;
+            console.log(h);
+            console.log(m);
+            console.log(s);
+            spans[0].innerHtml=Math.floor(h/10);
+            spans[1].innerHtml=s%10;
+            spans[3].innerHtml=Math.floor(h/10);
+            spans[4].innerHtml=s%10;
+            spans[6].innerHtml=Math.floor(h/10);
+            spans[7].innerHtml=s%10;
+            
+        },1000)
     }
 }
